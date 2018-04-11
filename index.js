d1ee5533dbfe096593c3c8bf0dfdd121003c5de7
CoderLim <779793115@qq.com>
2018-04-11 23:36:14 +0800

    :tada:

--- index.js
+++ index.js
@@ -2,17 +2,24 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 const MAIN_URL = 'http://www.shuwulou.com/shu/80.html';
+const CACHE_DIR = './cache';
 
 (async () => {
   try {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    // MBP的chrome
     page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
     await page.goto(MAIN_URL);
 
+    // 所有章节的索引
+    const episodeIndexes = await getEpisodeIndexes(page);
+    // console.log(episodeIndexes);
 
-    const novelList = await getNovelList(page);
-    console.log(novelList);
+    for (let episode of episodeIndexes) {
+      const episodeEx = await getEpisode(page, episode);
+      await writeEpisode(episodeEx);
+    }
 
     await browser.close();
   } catch (e) {
@@ -20,7 +27,7 @@ const MAIN_URL = 'http://www.shuwulou.com/shu/80.html';
   }
 })();
 
-async function getNovelList (page) {
+async function getEpisodeIndexes (page) {
   const novelListSelector = 'dd > a';
   await page.waitForSelector(novelListSelector);
   const links = await page.evaluate(selector => {
@@ -33,9 +40,25 @@ async function getNovelList (page) {
   return links;
 }
 
-async function download (page, episode) {
+async function getEpisode (page, episode) {
+  const contentSelector = '#content';
   const [title, url] = episode;
   await page.goto(url);
+  await page.waitForSelector(contentSelector);
+
+  const content = await page.evaluate(selector => {
+    const content = document.querySelector(selector);
+    return content.textContent;
+  }, contentSelector);
+
+  return [title, content, url];
+}
+
+async function writeEpisode (episode) {
+  let [title, content, url] = episode;
+  title = title.replace(' ', '-');
+
+  fs.writeFileSync(`${CACHE_DIR}/${title}`, content);
 }
 
 async function downloadEpisode(page, url) {
